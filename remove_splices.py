4dd51ff53909c74c095b9189a8c197bac3c050d3
Ben Liebeskind <bliebeskind@austin.utexas.edu>
2014-04-06 16:21:11 -0500

    Edited remove_splices to handle Matz lab style transcriptomes
    Wrote a new function longest_variants_matz. This uses a slightly different
    strategy than the others in that it builds a dictionary from all genes found
    in the whole file before flushing out longest variants. This has the advantage
    of not needing the genes to be next to eachother in the file, but may have the
    disadvantage of using more memory. Might be nice to work the flexibility part
    into the other functions with an option.
    
    Also edited get_longest by commenting out a useless piece of code. At least I
    hope it's useless. Better double check by re-running on Ensembl data again.

--- remove_splices.py
+++ remove_splices.py
@@ -6,11 +6,11 @@ import sys
 def get_longest(gene_D):
 	'''Given a dictionary mapping gene names to dictionaries of their protein 
 	products, each mapped to their length, return longest gene name'''
-	func = lambda x,y,D: x if D[x] >= D[y] else y
+	#func = lambda x,y,D: x if D[x] >= D[y] else y
 	for gene,prots in gene_D.iteritems():
 		func = lambda x,y: x if prots[x] >= prots[y] else y
 		longest = reduce(func,prots.keys())
-		return longest
+		yield longest
 	
 def longest_variants_ensembl(infile,format='fasta'):
 	'''Return generator of longest proteins from an Ensembl proteome. Will
@@ -25,14 +25,14 @@ def longest_variants_ensembl(infile,format='fasta'):
 		prot_len = len(str(rec.seq))
 		if gene in gene_map:
 			assert rec.description not in gene_map[gene],\
-			"Protein name %s found twice" % rec.description
+			"Protein name found twice %s" % rec.description
 			gene_map[gene][rec.description] = prot_len
 			record_map[rec.description] = rec
 		elif gene_map: # gene map is populated
 			assert gene not in all_genes, "Genes not continuous: %s" % gene
 			all_genes.append(gene)
-			longest = get_longest(gene_map) # get longest prot from previous 
-			yield record_map[longest]		# gene and yield it
+			longest = get_longest(gene_map).next() # get longest prot from  
+			yield record_map[longest]	# previous gene and yield it
 			gene_map,record_map = {},{} # flush dicts
 			gene_map[gene] = {rec.description:prot_len}
 			record_map[rec.description] = rec
@@ -40,7 +40,7 @@ def longest_variants_ensembl(infile,format='fasta'):
 			gene_map[gene] = {rec.description:prot_len}
 			record_map[rec.description] = rec
 			all_genes.append(gene)
-	longest = get_longest(gene_map) # get longest prot from last gene
+	longest = get_longest(gene_map).next() # get longest prot from last gene
 	yield record_map[longest]		# and yield it
 	sys.stderr.write("Found %i genes\n" % len(all_genes))
 	
@@ -57,14 +57,14 @@ def longest_variants_origins(infile,format='fasta'):
 		prot_len = len(str(rec.seq))
 		if gene in gene_map:
 			assert rec.description not in gene_map[gene],\
-			"Protein name %s found twice" % rec.description
+			"Protein name found twice: %s" % rec.description
 			gene_map[gene][rec.description] = prot_len
 			record_map[rec.description] = rec
 		elif gene_map: # gene map is populated
 			assert gene not in all_genes, "Genes not continuous: %s" % gene
 			all_genes.append(gene)
-			longest = get_longest(gene_map) # get longest prot from previous 
-			yield record_map[longest]		# gene and yield it
+			longest = get_longest(gene_map).next() # get longest prot from 
+			yield record_map[longest]	# previous gene and yield it
 			gene_map,record_map = {},{} # flush dicts
 			gene_map[gene] = {rec.description:prot_len}
 			record_map[rec.description] = rec
@@ -72,10 +72,36 @@ def longest_variants_origins(infile,format='fasta'):
 			gene_map[gene] = {rec.description:prot_len}
 			record_map[rec.description] = rec
 			all_genes.append(gene)
-	longest = get_longest(gene_map) # get longest prot from last gene
+	longest = get_longest(gene_map).next() # get longest prot from last gene
 	yield record_map[longest]		# and yield it
 	sys.stderr.write("Found %i genes\n" % len(all_genes))
 		
+		
+# Unlike above functions, doesn't flush the gene dict after each iteration
+# because the genes may not be sequential. This might not be much slower
+# than the strategy above, and is more flexible because it doesn't need
+# all the genes to be right next to eachother.
+def longest_variants_matz(infile,format='fasta'):
+	'''Return generator of longest sequence for each gene from a matz lab
+	style transcriptome'''
+	proteins = SeqIO.parse(infile,format)
+	gene_map = {} 	# {gene1: {prot1:24, prot2:109}}
+	record_map = {} # {seq.description: SeqRecord}
+	for rec in proteins:
+		gene = rec.description.split(' ')[1].strip().split("=")[1]
+		prot_len = len(str(rec.seq))
+		if gene in gene_map:
+			assert rec.description not in gene_map[gene],\
+			"Protein name found twice: %s" % rec.description
+			gene_map[gene][rec.description] = prot_len
+			record_map[rec.description] = rec
+		else: # gene map is not populated, i.e. first iteration
+			gene_map[gene] = {rec.description:prot_len}
+			record_map[rec.description] = rec
+	for longest in get_longest(gene_map):
+		yield record_map[longest]
+	sys.stderr.write("Found %i genes\n" % len(gene_map))
+
 if __name__ == '__main__':
 	infile = sys.argv[1]
 	outfile = sys.argv[2]
@@ -84,5 +110,7 @@ if __name__ == '__main__':
 		SeqIO.write(longest_variants_ensembl(infile,format='fasta'),outfile,'fasta')
 	elif database == 'origins':
 		SeqIO.write(longest_variants_origins(infile,format='fasta'),outfile,'fasta')
+	elif database == 'matz':
+		SeqIO.write(longest_variants_matz(infile,format='fasta'),outfile,'fasta')
 	else:
 		raise Exception("Database not found")
